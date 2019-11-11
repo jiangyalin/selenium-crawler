ec8306b4a3aca9e0bb68d4f241409c72a9e51608
yalin.jiang <yalin.jiang@71360.com>
2019-11-11 20:31:56 +0800

    自动编译指定目录下所有文件夹文压缩包

---
+++ comic/ddmmcc/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  akb49: {
+    name: '杀戮轮回',
+    url: 'http://ddmmcc.com/app/v.aspx?t=http://ddmmcc.com/vols/22019_153068/'
+  }
+}
---
+++ comic/ddmmcc/get-img.js
@@ -0,0 +1,43 @@
+const fs = require('fs')
+const Crawler = require('crawler')
+
+const updatedList = fs.readFileSync('./list.json', 'utf8')
+
+const node = JSON.parse(updatedList).node
+const map = {}
+fs.mkdirSync('./file/' + JSON.parse(updatedList).bookName)
+
+let crawler = new Crawler({
+  encoding: null, // 编码
+  maxConnections: 5, // 最大并发请求数
+  callback: (err, res, done) => {
+    if (err) console.log(err)
+    if (!err) console.log(res.options)
+    done()
+  }
+})
+const urls = []
+node.forEach((item, i) => {
+  let url = {
+    uri: item.href,
+    jQuery: false,
+
+    callback: (err, res, done) => {
+      if (err) console.log(err)
+      if (!err) main(res, item.name)
+      done()
+    }
+  }
+  urls.push(url)
+})
+
+let i = 100000
+
+const main = (res, name) => {
+  console.log('name', name)
+  i++
+  fs.writeFileSync('./file/word/' + name + '.jpg', res.body)
+}
+
+
+crawler.queue(urls)
