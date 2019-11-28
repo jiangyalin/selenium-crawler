c0925e08a8c6465a6b9f5cfc7fa063d47725931c
yalin.jiang <yalin.jiang@71360.com>
2019-11-28 14:42:18 +0800

    兼容页码信息错误

--- comic/wnacg/get-img-src-pc.js
+++ comic/wnacg/get-img-src-pc.js
@@ -17,6 +17,7 @@ async function example() {
       const _list = []
       await driver.get(url)
       for (let i = 0; i < 12; i++) {
+      // for (let i = 0; i < 1; i++) {
         const title = await driver.findElement(By.css('.cc li:nth-of-type(' + (i + 1) + ') .title a')).getText()
         const cover = await driver.findElement(By.css('.cc li:nth-of-type(' + (i + 1) + ') img')).getAttribute('src')
         const info = await driver.findElement(By.css('.cc li:nth-of-type(' + (i + 1) + ') .info_col')).getText()
@@ -36,18 +37,23 @@ async function example() {
       _list.forEach(item => {
         console.log('item', item)
       })
+      // 获取每一组
       for (let k = 0; k < _list.length; k++) {
         await driver.get(_list[k].src)
 
+        // 获取每一页
         for (let i = 0; i < Math.ceil(_list[k].size / 12); i++) {
           let size = (_list[k].size - i * 12) >= 12 ? 12 : _list[k].size - i * 12
 
+          // 获取每一张图片
           for (let j = 0; j < size; j++) {
-            const src = await driver.findElement(By.css('ul.cc li:nth-of-type(' + (j + 1) + ') img')).getAttribute('src')
-            _list[k].node.push({
-              page: i * 12 + j + 1,
-              src
-            })
+            if (await checkIsPresence(driver, 'ul.cc li:nth-of-type(' + (j + 1) + ') img')) {
+              const src = await driver.findElement(By.css('ul.cc li:nth-of-type(' + (j + 1) + ') img')).getAttribute('src')
+              _list[k].node.push({
+                page: i * 12 + j + 1,
+                src
+              })
+            }
           }
           if (i < Math.ceil(_list[k].size / 12) - 1) {
             const next = await driver.findElement(By.css('span.next a')).getAttribute('href')
@@ -60,6 +66,7 @@ async function example() {
       })
 
       save(list)
+      // n = 9999
     }
 
     console.log('正常结束')
@@ -69,6 +76,16 @@ async function example() {
     console.log('异常结束')
     driver.quit()
     save(list)
+  }
+}
+
+// 检查是否存在
+const checkIsPresence = async (driver, element) => {
+  try {
+    await driver.findElement(By.css(element))
+    return true
+  } catch  {
+    return false
   }
 }
 
