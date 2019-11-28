a1a81e782626e999479c11a03ba074b7efa93ce8
yalin.jiang <yalin.jiang@71360.com>
2019-11-28 18:05:54 +0800

    兼容页码信息错误

--- comic/wnacg/get-img-src.js
+++ comic/wnacg/get-img-src.js
@@ -1,52 +1,46 @@
 const {Builder, By, Key, until} = require('selenium-webdriver')
 const chrome = require('selenium-webdriver/chrome')
-const path = require('chromedriver').path
+const path = require('chromedriver').path // 必要，不能删除
 const fs = require('fs')
-const config = require('./config').akb49
+const config = require('./config').wxq
 
-const url = config.url
+// const url = config.url
+const key = config.key
+// const url = 'https://m.wnacg.org/albums-index-page-25-sname-%E7%84%A1%E9%82%AA%E6%B0%97%E6%BC%A2%E5%8C%96.html'
 
 async function example() {
-  const driver = new Builder().forBrowser('chrome').setChromeOptions(new chrome.Options().setMobileEmulation({deviceName: 'iPhone X'})).build()
+  const driver = new Builder().forBrowser('chrome').setChromeOptions(new chrome.Options().headless()).build()
   const list = []
   try {
-    // await driver.get('http://www.baidu.com')
+    const url = 'https://www.wnacg.org/?ctl=albums&page=' + n + '&sname=' + key
     await driver.get(url)
-    let index = 100000
-    for (let j = 0; j < 1000; j++) {
-      const text = await driver.findElement(By.css('.dv_pageindex')).getText()
-      const size = Number(text.split(' ')[2])
-      for (let i = 0; i < size; i++) {
-        const src = await driver.findElement(By.css('.dv_img img')).getAttribute('src')
-        list.push({
-          href: src,
-          name: index++
-        })
-        console.log('src', src)
-        const js = 'document.querySelector(\'.cViewPChange1.cNext\').click()'
-        if (i < size - 1) driver.executeScript(js)
-        await driver.sleep(200)
-      }
-      const js = 'document.querySelector(\'.cViewPChange1.cNextVol\').click()'
-      driver.executeScript(js)
-      await driver.sleep(1000)
-    }
-    fs.writeFileSync('./list.json', JSON.stringify({
-      tips: '章节',
-      bookName: config.name,
-      node: list
-    }))
-    console.log('正常结束')
 
+    console.log('正常结束')
+    driver.quit()
+    save(list)
   } finally {
-    fs.writeFileSync('./list.json', JSON.stringify({
-      tips: '章节',
-      bookName: config.name,
-      node: list
-    }))
     console.log('异常结束')
-    // driver.quit()
+    driver.quit()
+    save(list)
   }
+}
+
+// 检查是否存在
+const checkIsPresence = async (driver, element) => {
+  try {
+    await driver.findElement(By.css(element))
+    return true
+  } catch  {
+    return false
+  }
+}
+
+const save = node => {
+  fs.writeFileSync('./list.json', JSON.stringify({
+    tips: '章节',
+    bookName: config.name,
+    node: node
+  }))
 }
 
 example()
